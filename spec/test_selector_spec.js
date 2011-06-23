4c6e4563d3d395dde53b0ef0ecc32c39eefda13f
Matteo Collina <matteo.collina@gmail.com>
2011-06-23 14:55:22 +0200

    Added basic coffeescript support.

--- spec/test_selector_spec.js
+++ spec/test_selector_spec.js
@@ -18,4 +18,18 @@ describe('test selector', function() {
       expect(selector.select(process.cwd() + '/path/to/somefile')).toBeUndefined();
     });
   });
+
+  describe('find coffeescripts', function() {
+    it('runs a single spec if thats all that changed', function() {
+      expect(selector.select(process.cwd() + '/path/to/some_spec.coffee')).toEqual(['path/to/some_spec.coffee']);
+    });
+
+    it('runs all specs if any other javascript file has changed', function() {
+      expect(selector.select(process.cwd() + '/path/to/file.coffee')).toEqual(['spec']);
+    });
+
+    it('runs no specs if an non-javascript file changes', function() {
+      expect(selector.select(process.cwd() + '/path/to/somefile')).toBeUndefined();
+    });
+  });
 });
